ae3b680c04960038bd3a10a33932060c6b1a3985
SilvanaLF <sfcreativa@gmail.com>
2023-03-23 05:13:54 -0300

    stats funciona. Details no

--- assets/js/details.js
+++ assets/js/details.js
@@ -5,7 +5,10 @@ createApp({
          return{
           arrayEventos: [],
           nombreIngresado: '',
-          eventosFiltro: [],    
+          eventObjet: {},
+          arrayCategorias: [],
+          dupCats: [],
+          checked: [],    
          }
     },
     created(){
@@ -13,10 +16,15 @@ createApp({
          fetch("https://mindhub-xj03.onrender.com/api/amazing")
          .then(response => response.json())
          .then(data => {
-                this.arrayEventos = data.events
-                const id = new URLSearchParams(location.search).get("id");
-                console.log(id);
+                this.arrayEventos = data.events;
+                const queryString = location.search
+                const params = new URLSearchParams(queryString)
+                this.id = params.get("id")
+                this.eventObjet = this.arrayEventos.find(evento => evento._id == this.id);
+                console.log(this.eventObjet);
+
                 
+        
          })
          .catch(error => console.log(error))
     },
@@ -25,33 +33,4 @@ createApp({
     
 }).mount("#app")
 
-/*    
-function mostrarCard(eventObjet) {
-
-    const container = document.getElementById('detailsCard')
-    let nuevoDiv = document.createElement('div')
-    nuevoDiv.className = "card mt-3 mb-3"
-    nuevoDiv.style.width ="18rem"
-    nuevoDiv.innerHTML = `<img src="${eventosFiltro.image}" class="card-img-top" style="height: 150px" alt="Race">
-        <div class="card-body d-flex flex-column justify-content-between">
-        <h3 class="card-title">${eventosFiltro.name}</h3>
-        <p class="card-text">Category: ${eventosFiltro.category}</p>
-        <p class="card-text">Date: ${eventosFiltro.date}</p>
-        <p class="card-text">${eventosFiltro.description}</p>
-        <p class="card-text">Place: ${eventosFiltro.place}</p>
-        <p class="card-text">Capacity: ${eventosFiltro.capacity}</p>
-        <p class="card-text">Assistance: : ${eventosFiltro.assistance ? eventosFiltro.assistance : eventosFiltro.estimate}</p>
-        </div>`
-        container.appendChild(nuevoDiv)
-    let botonInicio = document.createElement('button')
-    botonInicio.className = "btn btn-dark nav-item p-2 me-1 ms-1 mb-1"
-    botonInicio.style = "color: #e0046c; background-color: #e9ecef"
-    botonInicio.textContent = "Back to Home"
-    botonInicio.addEventListener('click', () => {
-    window.location.href = "./index.html"
-    })
-        nuevoDiv.appendChild(botonInicio)
-
-    }
-   */
 
--- assets/js/stats.js
+++ assets/js/stats.js
@@ -1,86 +1,75 @@
-async function traerEvents() {
-    try {
-      const response = await fetch('https://mindhub-xj03.onrender.com/api/amazing')
-      console.log(response)
-      const data = await response.json()
-      console.log(data)
-      const events = data.events
-      console.log(events)
-  
-      const table = document.getElementById("table1")
-      mostrarTable(events, table)
-  
-      let filAssis = events.filter (event => event.assistance)
-      let filEstim = events.filter (event => event.estimate)
-      mostrarTableA(filEstim, tableGroup2)
-      mostrarTableA(filAssis, tableGroup3)
-  
-  
-      function mostrarTable(array, container) {
-  
-        let attendanceHigh = array.filter(event => event.assistance).reduce((event1, event2) => 
-           (event1.assistance / event1.capacity) > (event2.assistance / event2.capacity)? event1 : event2
-        )
-  
-        let attendanceLow = array.filter(event => event.assistance).reduce((event1, event2) => 
-          (event1.assistance / event1.capacity) < (event2.assistance / event2.capacity)? event1: event2
-        )
-  
-        let capacityLarger = array.reduce((event1, event2) => (event1.capacity >= event2.capacity)?  event1 : event2
-        )
-  
-  
-        let newTr = document.createElement('tr')
-        newTr.innerHTML = `
-                  <td><b>${attendanceHigh.name}:</b> ${(attendanceHigh.assistance / attendanceHigh.capacity * 100).toFixed(2)} %</td>
-                  <td><b>${attendanceLow.name}:</b> ${(attendanceLow.assistance / attendanceLow.capacity * 100).toFixed(2)} %</td>
-                  <td><b>${capacityLarger.name}:</b> ${capacityLarger.capacity}</td>`
-  
-        container.appendChild(newTr)
-      }
-  
-      function ingresos(array, categoryName) {
-  
-        let byCategory = array.filter(event => event.category == categoryName)
-        let byEvents = byCategory.reduce((suma, event) => event.assistance != undefined ? suma += event.assistance * event.price : suma += event.estimate * event.price, 0)
-        return byEvents
-      }
-  
-      function porcentajeAsistencia(array, categoryName) {
-  
-        let byCategory = array.filter(event => event.category == categoryName)
-        console.log(byCategory)
-        let byEvents = byCategory.reduce((total, event) => event.assistance != undefined ? total += (event.assistance / event.capacity) * 100 : total += (event.estimate / event.capacity)*100, 0)
-        console.log(byEvents)
-        return (byEvents / byCategory.length).toFixed(2)
-      }
-  
-      function mostrarTableA(array, container) {
-  
-        let categories = [... new Set(array.map(event => event.category))]
-        console.log(categories)
-  
-  
-        let fragment = document.createDocumentFragment()
-  
-        for (let category of categories) {
-          let newTr = document.createElement('tr')
-          newTr.innerHTML = `
-                        <td><b>${category}</b></td>
-                        <td> &#36 ${ingresos(array, category)}</td>
-                        <td>${porcentajeAsistencia(array, category)} %</td>`
-  
-          fragment.appendChild(newTr)
-        }
-        container.appendChild(fragment)
-  
-      }
-  
+const { createApp } = Vue
+createApp({
+  //define el estado de la aplicación
+  data() {
+    return {
+      datos: {},
+      dataEvents: [],
+      upcoming: [],
+      past: [],
+      tableUpcoming: [],
+      tablePast: [],
+      statistics: []
     }
-    catch (error) {
-        console.log(error = "No se ha logrado traer la informacion de la API")
+  },
+  created() {
+    fetch("https://mindhub-xj03.onrender.com/api/amazing")
+      .then(response => response.json())
+      .then(datos => {
+        this.datos = datos
+        this.dataEvents = datos.events
+        this.upcoming = datos.events.filter(event => new Date(event.date) > new Date(this.datos.currentDate))
+        this.past = datos.events.filter(event => new Date(event.date) < new Date(this.datos.currentDate))
+        this.statistics = this.tabla1(this.dataEvents)
+        this.tablasPorCategoria(this.upcoming, this.tableUpcoming)
+        this.tablasPorCategoria(this.past, this.tablePast)
+
+
+      })
+      .catch(error => console.log(error.message))
+
+  },
+  methods: {
+    tabla1(eventos) {
+      let moreAssistance = eventos.filter(event => event.assistance).reduce((evento1, evento2) =>
+        (evento1.assistance / evento1.capacity) > (evento2.assistance / evento2.capacity) ? evento1 : evento2
+      )
+      let lessAssistance = eventos.filter(event => event.assistance).reduce((evento1, evento2) =>
+        (evento1.assistance / evento1.capacity) < (evento2.assistance / evento2.capacity) ? evento1 : evento2
+      )
+      let capacity = eventos.reduce((evento1, evento2) =>
+        evento1.capacity > evento2.capacity ? evento1 : evento2
+      )
+      return [moreAssistance, lessAssistance, capacity]
+    },
+    revenue(eventos) {
+      return eventos.reduce((total, event) => {
+        event.assistance == undefined ? total += (event.estimate * event.price) : total += (event.assistance * event.price)
+        return total
+      }, 0)
+    },
+    porcentaje(eventos) {
+      return (eventos.reduce((total, event) => {
+        event.assistance == undefined ? total += (event.estimate / event.capacity) : total += (event.assistance / event.capacity)
+        return total
+      }, 0) * 100 / eventos.length).toFixed(2)
+    },
+    tablasPorCategoria(eventos, table) {
+      let categorias = [...new Set(eventos.map(evento => evento.category))]
+      categorias.forEach(category => {
+        let objet = {}
+        objet.category = category
+        let eventsByCategory = eventos.filter(event => event.category == category)
+        objet.revenue = this.revenue(eventsByCategory)
+
+        objet.porcentaje = this.porcentaje(eventsByCategory)
+
+        table.push(objet)
+
+      })
+      table.sort((a,b)=>a.category>b.category)
     }
+
   }
-  
-  traerEvents()
-  
+
+}).mount('#app')
